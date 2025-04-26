7ea1c6ec0a60bd80f023dab0048c3e96127eae65
WRNGx3 <WRNGx3@LAPTOP-AZ-002>
2025-04-26 11:57:54 +0300

    Setup PostCSS processing

---
+++ BlazoremonHub/postcss.config.js
@@ -0,0 +1,7 @@
+﻿module.exports = ({ env }) => ({
+    plugins: [
+        require('autoprefixer'),
+        env === 'production' ? require('cssnano')({ preset: 'default' }) : false
+    ].filter(Boolean),
+    map: env === 'production' ? false : 'inline'
+});
