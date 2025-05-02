0c89d14ac4007af4049b97a27aad181487747e2d
Andrei Zhvaleuski <andrei@aiz-dev.ru>
2025-05-02 23:51:18 +0300

    Support dark theme

--- BlazoremonHub/Layout/MainLayout.razor.cs
+++ BlazoremonHub/Layout/MainLayout.razor.cs
@@ -1,7 +1,26 @@
 ﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
 
 namespace BlazoremonHub.Layout;
 
 public partial class MainLayout : LayoutComponentBase
 {
+    private bool _isDarkMode;
+    private MudThemeProvider _mudThemeProvider = default!;
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            await _mudThemeProvider.WatchSystemPreference(OnSystemPreferenceChanged);
+            StateHasChanged();
+        }
+    }
+
+    private Task OnSystemPreferenceChanged(bool isDarkMode)
+    {
+        _isDarkMode = isDarkMode;
+        StateHasChanged();
+        return Task.CompletedTask;
+    }
 }
